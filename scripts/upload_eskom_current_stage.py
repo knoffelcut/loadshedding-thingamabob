6349a8530b4d318a302a876b0b637919b2f40354
knoffelcut <hardus_richter@hotmail.com>
2022-04-08 18:31:51 +0200

    Lambda function: upload eskom stage

--- scripts/upload_eskom_current_stage.py
+++ scripts/upload_eskom_current_stage.py
@@ -1,11 +1,11 @@
+
+import argparse
 import datetime
 
 import scraping.scraping
 import loadshedding_coct_stage_query.query_and_upload
 
-if __name__ == '__main__':
-    import argparse
-
+def get_parser():
     parser = argparse.ArgumentParser(description='Uploads the current national loadshedding stage as reported by Eskom')
     parser.add_argument('--url', type=str, default='http://loadshedding.eskom.co.za/LoadShedding/GetStatus',
         help='Eskom API URL to get the current stage'
@@ -22,6 +22,11 @@ if __name__ == '__main__':
     parser.add_argument('--attempts', type=int, default=5,
         help='Number of times to attempt the HTTP call.'
         )
+
+    return parser
+
+if __name__ == '__main__':
+    parser = get_parser()
     args = parser.parse_args()
 
     loadshedding_coct_stage_query.query_and_upload.query_and_upload(
@@ -30,3 +35,23 @@ if __name__ == '__main__':
         f_scrape=scraping.scraping.extract_eskom_loadshedding_stage,
         f_datapack=lambda x: x
         )
+
+def lambda_handler(event, context):
+    #TODO Move to a module (the function to interpret the event object as arguments)
+    parser = get_parser()
+
+    events = [s for k, v in event.items() for s in (f'--{k}', f'{v}')]
+
+    args = parser.parse_args(events)
+
+    loadshedding_coct_stage_query.query_and_upload.query_and_upload(
+        **vars(args),
+        suffix='current-stage',
+        f_scrape=scraping.scraping.extract_eskom_loadshedding_stage,
+        f_datapack=lambda x: x
+        )
+
+    return {
+        "statusCode": 200,
+        "body": '{}',
+    }
