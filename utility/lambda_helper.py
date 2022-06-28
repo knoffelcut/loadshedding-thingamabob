3091a9270a312721d219a58e6e58d9ff32eab694
knoffelcut <hardus_richter@hotmail.com>
2022-06-28 21:11:56 +0200

    Fix AWS Event info misinterpret as args
    In particular, event['region']
    was interpreted as args.region_loadshedding

--- utility/lambda_helper.py
+++ utility/lambda_helper.py
@@ -1,6 +1,9 @@
 import argparse
 
 def parse_events_as_args(parser: argparse.ArgumentParser, event: dict):
+    # Other event['region'] may be interpreted as 'region_loadshedding'
+    parser.allow_abbrev = False
+
     events = [s for k, v in event.items() for s in (f'--{k}', f'{v}')]
 
     args, _ = parser.parse_known_args(events)
