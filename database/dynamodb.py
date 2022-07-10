34542faaf0e76fcd20c312010d431ee178bd79a2
knoffelcut <hardus_richter@hotmail.com>
2022-07-10 19:59:26 +0200

    CoCT and national schedule parsing functions

--- database/dynamodb.py
+++ database/dynamodb.py
@@ -4,11 +4,9 @@ import boto3
 class EmptyDynamoResponse(Exception):
     pass
 
-# From https://stackoverflow.com/a/12809659
-#  and https://blog.yadutaf.fr/2012/10/07/common-dynamodb-questionsmisconceptionsrecipes/
-def get_most_recent_scraped_data(table, primary_key: str):
+def get_recent_scraped_data(table, primary_key: str, n_entries: int=1, strict: bool=False):
     response = table.query(
-        KeyConditionExpression=boto3.dynamodb.conditions.Key('field').eq(primary_key), ScanIndexForward=False, Limit=1
+        KeyConditionExpression=boto3.dynamodb.conditions.Key('field').eq(primary_key), ScanIndexForward=False, Limit=n_entries
         )
     assert response['ResponseMetadata']['HTTPStatusCode'] == 200
 
@@ -17,7 +15,14 @@ def get_most_recent_scraped_data(table, primary_key: str):
     if not items:
         raise EmptyDynamoResponse(f"No items in table='{table.name}' matching primary_key='{primary_key}'")
 
-    assert len(items) == 1
+    assert (not strict) or len(items) == n_entries
+
+    return items
+
+# From https://stackoverflow.com/a/12809659
+#  and https://blog.yadutaf.fr/2012/10/07/common-dynamodb-questionsmisconceptionsrecipes/
+def get_most_recent_scraped_data(table, primary_key: str):
+    items = get_recent_scraped_data(table, primary_key, 1, True)
 
     item = items[0]
     assert item['field'] == primary_key
---
+++ parsing/parsing.py
@@ -0,0 +1,139 @@
+import datetime
+import json
+
+import loadshedding_thingamabob.schedule
+
+class ParseError(ValueError):
+    pass
+
+def extract_national_loadshedding_stage(response_text: str):
+    """Extracts the current Eskom loadshedding stage
+
+    Args:
+        response_text (str): Eskom API response
+
+    Returns:
+        int: Current national loadshedding stage
+    """
+    try:
+        stage = int(response_text)
+        stage = stage - 1
+        return stage
+    except Exception as e:
+        raise ParseError from e
+
+def convert_national_plaintext_to_schedule(timestamp_db: int, plaintext: str):
+    if isinstance(plaintext, str):
+        plaintext = extract_national_loadshedding_stage(plaintext)
+    assert isinstance(plaintext, int)
+
+    schedule = [
+        (timestamp_db, plaintext),
+    ]
+    schedule = loadshedding_thingamabob.schedule.Schedule(schedule)
+
+    return schedule
+
+def convert_coct_plaintext_to_schedule(timestamp_db: int, plaintext: str):
+    assert isinstance(plaintext, str)
+
+    data = json.loads(plaintext)
+    data = data[0]
+
+    # Parsing of lastUpdated ain't perfect here (note https://stackoverflow.com/a/30696682)
+    #   since we throw away the timezone information
+    #   but good enough for now, since we only need to parse a single and consistent format
+    # last_updated is not used right now, but it can be used when last_updated < start_time
+    #   In that case we actually need to get the previous schedule and get the current stage
+    #   for the epoch:start_start stage
+    assert data['lastUpdated'][-1].lower() == 'z'
+    last_updated = datetime.datetime.fromisoformat(data['lastUpdated'][:-1])
+
+    start_time = datetime.datetime.fromisoformat(data['startTime']).timestamp()
+    current_stage = int(data['currentStage'])
+    next_stage_start_time = datetime.datetime.fromisoformat(data['nextStageStartTime']).timestamp()
+    next_stage = int(data['nextStage'])
+
+    schedule = [
+        (start_time, current_stage),
+        (next_stage_start_time, next_stage),
+    ]
+    schedule = loadshedding_thingamabob.schedule.Schedule(schedule)
+
+    return schedule
+
+# TODO Move to external development script
+def development_coct():
+    import pickle
+    import datetime
+
+    try:
+        # raise FileNotFoundError
+        with open('cache_coct_plaintext.pkl', 'rb') as f:
+            items  = pickle.load(f)
+    except FileNotFoundError as e:
+        import boto3
+        import database.dynamodb
+
+        region_loadshedding = 'coct'
+        suffix = 'plaintext'
+        table_name = 'loadshedding'
+
+        dynamodb = boto3.resource('dynamodb', region_name='af-south-1')
+        table = dynamodb.Table(table_name)
+
+        partition_key = f"{region_loadshedding}-{suffix}"
+        items = database.dynamodb.get_recent_scraped_data(table, partition_key, 128, False)
+
+        with open('cache_coct_plaintext.pkl', 'wb') as f:
+            pickle.dump(items, f, pickle.HIGHEST_PROTOCOL)
+
+    for i, item in enumerate(items[::-1]):
+        timestamp = int(item['timestamp'])
+        data = item['data']
+
+        schedule = convert_coct_plaintext_to_schedule(timestamp, data)
+
+        print(f'{i:02d} {timestamp} {datetime.datetime.fromtimestamp(timestamp)}')
+        print(data)
+        print(schedule)
+
+# TODO Move to external development script
+def development_national():
+    import pickle
+    import datetime
+
+    try:
+        # raise FileNotFoundError
+        with open('cache_national_plaintext.pkl', 'rb') as f:
+            items  = pickle.load(f)
+    except FileNotFoundError as e:
+        import boto3
+        import database.dynamodb
+
+        region_loadshedding = 'national'
+        suffix = 'plaintext'
+        table_name = 'loadshedding'
+
+        dynamodb = boto3.resource('dynamodb', region_name='af-south-1')
+        table = dynamodb.Table(table_name)
+
+        partition_key = f"{region_loadshedding}-{suffix}"
+        items = database.dynamodb.get_recent_scraped_data(table, partition_key, 128, False)
+
+        with open('cache_coct_plaintext.pkl', 'wb') as f:
+            pickle.dump(items, f, pickle.HIGHEST_PROTOCOL)
+
+    for i, item in enumerate(items[::-1]):
+        timestamp = int(item['timestamp'])
+        data = item['data']
+
+        schedule = convert_national_plaintext_to_schedule(timestamp, data)
+
+        print(f'{i:02d} {timestamp} {datetime.datetime.fromtimestamp(timestamp)}')
+        print(data)
+        print(schedule)
+
+
+if __name__ == '__main__':
+    development_national()
