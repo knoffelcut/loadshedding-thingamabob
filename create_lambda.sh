e41a921c4495ccb7f89eb8d2686497f6e252a12d
knoffelcut <hardus_richter@hotmail.com>
2022-07-17 15:28:22 +0200

    Exclude cached Python files from lambda packages
    Additionally, skip the `pip install -r requirements.txt` step
    if no requirements are specified

--- create_lambda.sh
+++ create_lambda.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # Based on https://github.com/m3h/loadshedding-stage-api/blob/master/1_create_lambda_zip.sh
 
+set -e
+
 FUNCTION_FILE=${1:-"scripts/upload_eskom_current_stage.py"}
 LAMBDA_NAME=${2:-"upload_eskom_current_stage"}
 
@@ -9,10 +11,12 @@ STAGING_DIR=./${LAMBDA_NAME}-aws-deploy
 
 rm -rf "$OUTPUT_ZIP" "$STAGING_DIR"
 mkdir "$STAGING_DIR"
-cp -r ./database ./loadshedding_thingamabob ./scraping ./utility "$STAGING_DIR"
+# Only include .py file in modules (exclude cache etc.)
+# https://stackoverflow.com/a/11111793
+rsync -a --include="*/" --include="*.py" --exclude="*" ./database ./loadshedding_thingamabob ./parsing ./utility "$STAGING_DIR"
 cp -r ${FUNCTION_FILE} "${STAGING_DIR}/lambda_function.py"
 
-python3.8 -m pip install --target "$STAGING_DIR" -r ./requirements.txt
+[ -s ./requirements.txt ] && echo python3.8 -m pip install --target "$STAGING_DIR" -r ./requirements.txt
 cd "$STAGING_DIR"
 zip -r "../$OUTPUT_ZIP" .
 cd ..
